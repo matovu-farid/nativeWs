9a86a321e8c9775ab5c5e4d9c81e830bcceb111b
matovu-farid <matovu90@gmail.com>
2022-08-05 00:46:52 +0300

    Styled basic chat app

--- chat.js
+++ chat.js
@@ -7,9 +7,7 @@ app.use(express.static(__dirname + '/public'))
 const expressServer = app.listen(8000)
 const io = socketio(expressServer)
 io.on('connection',(socket)=>{
-  // socket.emit('messageFromServer',{data: "Welcome to the socketio server"})
   socket.on('messageToServer',(dataFromClient)=>{
-    // console.log(dataFromClient)
 
   })
   socket.on('ChatFormClient',(msg)=>{
